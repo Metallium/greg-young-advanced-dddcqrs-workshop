fecf940df6e42d88872c5505345d85403cb083ad
Nikita <dev.nkh@gmail.com>
2016-06-01 17:24:49 +0300

    create ten orders to focus on blocking issue

--- src/Restaurant/Bootstrapper.cs
+++ src/Restaurant/Bootstrapper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Restaurant
 {
@@ -7,18 +8,24 @@ namespace Restaurant
     {
         public static void Main()
         {
-            var orderId = new Waiter(
-                new Cook(
-                    new AssistantManager(
-                        new Cashier(new Printer())
+            var ordersCount = 10;
+            for (var i = 0; i < ordersCount; ++i)
+            {
+                var orderId = new Waiter(
+                    new Cook(
+                        new AssistantManager(
+                            new Cashier(new Printer())
+                            )
                         )
                     )
-                )
-                .PlaceNewOrder(new Dictionary<string, int>
-                {
-                    {"meat", 2}
-                });
-            Console.WriteLine($"Placed an order {orderId}.");
+                    .PlaceNewOrder(new Dictionary<string, int>
+                    {
+                        {"meat", 2}
+                    });
+                Console.WriteLine($"Placed an order {orderId}.");
+
+            }
+            Console.WriteLine("Placed all orders.");
             Console.ReadKey(false);
         }
 
